18069f33826baf88c8c12f5fbf3a41eb7aa8948b
ShiryAbdo <shaimaaenfikry@gmail.com>
2017-11-01 00:30:39 +0200

    6

--- app/src/main/java/com/Inspira/odo/sellerUi/ContinuingRegSeler.java
+++ app/src/main/java/com/Inspira/odo/sellerUi/ContinuingRegSeler.java
@@ -1,5 +1,6 @@
 package com.Inspira.odo.sellerUi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +9,20 @@ import android.widget.Button;
 import com.Inspira.odo.R;
 
 public class ContinuingRegSeler extends AppCompatActivity {
-    Button creatSelerAcout ;
+    Button Campany_acount_Countio ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_continuing_reg_seler);
+        Campany_acount_Countio = (Button)findViewById(R.id.Campany_acount_Countio);
+        Campany_acount_Countio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContinuingRegSeler.this,WorkingOnOne.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/Inspira/odo/sellerUi/WorkingOnOne.java
@@ -0,0 +1,59 @@
+package com.Inspira.odo.sellerUi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.Inspira.odo.R;
+
+import java.util.ArrayList;
+
+public class WorkingOnOne extends AppCompatActivity {
+    ArrayList<String> categories;
+    Spinner SpinnerOne ,SpinnerTwo ,SpinnerThree  ,SpinnerCarFoure;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_working_on_one);
+
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        setTitle(Html.fromHtml("<small>Working on</small>"));
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        SpinnerOne = (Spinner)findViewById(R.id.SpinnerOne);
+        SpinnerTwo=(Spinner)findViewById(R.id.SpinnerTwo);
+        SpinnerThree =(Spinner)findViewById(R.id.SpinnerThree);
+        SpinnerCarFoure =(Spinner)findViewById(R.id.SpinnerCarFoure);
+
+
+        // Spinner Drop down elements
+        categories= new ArrayList<>();
+        categories.add("Automobile");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinnerOne.setAdapter(dataAdapter);
+        SpinnerTwo.setAdapter(dataAdapter);
+        SpinnerThree.setAdapter(dataAdapter);
+        SpinnerCarFoure.setAdapter(dataAdapter);
+
+
+
+    }
+}
